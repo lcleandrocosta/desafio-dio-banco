3d9b12430004139a5f4398c500170c39a068ad1d
lcleandrocosta <lcleandrocosta@gmail.com>
2022-03-23 09:56:55 -0300

    Criar classe banco e pacote dio.banco.desafio

---
+++ banco/src/dio/banco/desafio/Banco.java
@@ -0,0 +1,16 @@
+package dio.banco.desafio;
+
+public class Banco {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
